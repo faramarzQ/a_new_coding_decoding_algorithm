967ebfea8cae724b398ee76d1ac6dc80ad0d8f9e
faramarz <farmamarzqoshchi@gmail.com>
2021-02-02 17:10:18 +0330

    finished, comments added

--- decode.py
+++ decode.py
@@ -1,47 +1,128 @@
 import math
 
 def do(base_table, matrix):
+    """encoded the given matrix into a string
 
+    Args:
+        base_table ([dict]): [maps letters to integers]
+        matrix ([list]): [2d encoded matrix]
+
+    Returns:
+        [string]: [decoded string]
+    """
+
+    # find the Q-matrix for the problem
     q_matrix = calculateQMatrix(matrix)
+
+    # calculate the E1 matrix
     first_element_matrix = calculateFirstElementMatrix(q_matrix, matrix)
+
+    # calculate the E2 matrix
     second_element_matrix = calculateSecondElementMatrix(q_matrix, matrix)
+
+    # find the latent indexes in the encoded matrix
     latent_indexes = findLatentIndexes(matrix, first_element_matrix, second_element_matrix, q_matrix)
+
+    # reshape each blocks using the latent indexes
     reshaped_blocks = reshapeBlocksWithLatentIndexes(latent_indexes, matrix)
+
+    # reconstruct the matrix of integers using the reshaped blocks
     reconstructed_matrix = reconstructMatrix(base_table, reshaped_blocks)
-    string = convertNumberMatrixToString(reconstructMatrix)
+
+    # map back the integers into string of chars
+    string = convertNumberMatrixToString(base_table, reconstructed_matrix)
+
+    return string
+pass
 
 def calculateQMatrix(matrix):
+    """calculates the Q-Matrix for the problem
 
+    Args:
+        matrix ([list]): [2d encoded matrix]
+
+    Returns:
+        [list]: [q-matrix]
+    """
+
+    # the condition mentioned in the paper
     if(len(matrix) <= 3):
         n = 3
     else:
         n = len(matrix)
 
+    # create and fill the matrix with zeros
     q_matrix = [0]*4
     q_matrix = [fibonacci(n+1), fibonacci(n), fibonacci(n), fibonacci(n-1)]
+
     return q_matrix
+pass
 
 def fibonacci(n):
+    """a recursive fibonacci funciton
+
+    Args:
+        n ([integer]): [an integer]
+
+    Returns:
+        [integer]: [fibonacci of the given integer]
+    """
     if n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
+pass
 
 def calculateFirstElementMatrix(q_matrix, matrix):
+    """finds the E1 matrix of the encoded matrix mentioned in the paper
+
+    Args:
+        q_matrix ([list]): [Q-matrix]
+        matrix ([list]): [the encoded matrix]
+
+    Returns:
+        [list]: [the E1 matrix]
+    """
+    # create and fill the matrix with zeros, then calculate it using the mentioned formula
     element_matrix = [0]*len(matrix)
     for i in range(len(element_matrix)):
         element_matrix[i] = q_matrix[0]*matrix[i][1] + q_matrix[2]*matrix[i][2]
 
     return element_matrix
+pass
 
 def calculateSecondElementMatrix(q_matrix, matrix):
+    """finds the E2 matrix of the encoded matrix mentioned in the paper
+
+    Args:
+        q_matrix ([list]): [Q-matrix]
+        matrix ([list]): [the encoded matrix]
+
+    Returns:
+        [list]: [the E2 matrix]
+    """
+    # create and fill the matrix with zeros, then calculate it using the mentioned formula
     element_matrix = [0]*len(matrix)
     for i in range(len(element_matrix)):
         element_matrix[i] = q_matrix[1]*matrix[i][1] + q_matrix[3]*matrix[i][2]
 
     return element_matrix
+pass
 
 def findLatentIndexes(matrix, first_element_matrix, second_element_matrix, q_matrix):
+    """find latent indexes in the blocks using the mentioned formula in the paper
+
+    Args:
+        matrix ([list]): [the encoded matrix]
+        first_element_matrix ([list]): [E1 matrix]
+        second_element_matrix ([list]): [E2 matrix]
+        q_matrix ([list]): [Q-matrix]
+
+    Returns:
+        [list]: [list of latent indexes for each block]
+    """
+
+    # create and fill a list with zeros, the calculate the latent indexes using the formula
     n = len(matrix)
     latent_indexes = [0]*len(matrix)
     for i in range(len(matrix)):
@@ -53,10 +134,21 @@ def findLatentIndexes(matrix, first_element_matrix, second_element_matrix, q_mat
         latent_indexes[i] = temp_x
 
     return latent_indexes
+pass
 
 def reshapeBlocksWithLatentIndexes(latent_indexes, matrix):
-    reshaped_blocks = [ [ 0 for i in range(len(matrix[0])) ] for j in range(len(matrix)) ]
+    """reshape the encoded matrix and put the latent indexes in their proper positions
 
+    Args:
+        latent_indexes ([list]): [list of latent indexes for each blocks]
+        matrix ([list]): [2d encoded matrix]
+
+    Returns:
+        [list]: [2d matrix, reshaped blocks]
+    """
+
+    # create and fill a 2d matrix, and reshape it
+    reshaped_blocks = [ [ 0 for i in range(len(matrix[0])) ] for j in range(len(matrix)) ]
     for i in range(len(matrix)):
         reshaped_blocks[i][0] = matrix[i][1]
         reshaped_blocks[i][1] = matrix[i][2]
@@ -64,14 +156,23 @@ def reshapeBlocksWithLatentIndexes(latent_indexes, matrix):
         reshaped_blocks[i][3] = matrix[i][3]
 
     return reshaped_blocks
+pass
 
 def reconstructMatrix(base_table, reshaped_blocks):
+    """reconstruct the matrix by reshaped blocks
 
+    Args:
+        base_table ([dict]): [maps letters to integers]
+        reshaped_blocks ([list]): [reshaped blocks]
+
+    Returns:
+        [list]: [the reconstruced 2d matrix]
+    """
     blocks_per_row = int(math.sqrt(len(reshaped_blocks)))
     blocks = blocks_per_row * 2
 
+    # create and fill a matrix with zeros, put reshaped blocks in their proper position
     reconstructed_matrix = [ [ (base_table['0']+blocks) for i in range(blocks) ] for j in range(blocks) ]
-
     for i in range(len(reshaped_blocks)):
         for j in range(4):
             row = int( 2*math.floor(i/blocks_per_row) + math.floor(j/2) )
@@ -79,4 +180,34 @@ def reconstructMatrix(base_table, reshaped_blocks):
             reconstructed_matrix[row][col] = reshaped_blocks[i][j]
 
     return reconstructed_matrix
+pass
 
+def convertNumberMatrixToString(base_table, matrix):
+    """mapp the reconstruced matrix of integers into characters using the base table
+
+    Args:
+        base_table ([dict]): [maps letters to integers]
+        [list]: [the reconstruced 2d matrix]
+
+    Returns:
+        [string]: [decoded string]
+    """
+    # calculate n variable mentioned in the paper
+    n = math.ceil(math.sqrt(len(matrix))*2)
+
+    if(n <= 3):
+        b = 3
+    else:
+        b = n
+
+    # declare and map back the integers in matrix to string of characters
+    string = ''
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            string += str(list(base_table.keys())[list(base_table.values()).index( matrix[i][j] - b )])
+
+    # replace the zeros with spaces
+    string = string.replace('0', ' ')
+
+    return string
+pass
--- encode.py
+++ encode.py
@@ -1,40 +1,118 @@
 import math
-import numpy
 
 def do(base_table, string):
-    converted_string = convertStringToInteger(base_table, string)
-    string_matrix = createStringMatrix(base_table, converted_string)
-    print(string_matrix)
-    blocks = splitStringMatrix(string_matrix)
+    """encrypt the given string using the base table letters
+
+    Args:
+        base_table ([dict]): [maps letters to integers]
+        string ([string]): [the string given to encode]
+
+    Returns:
+        [list]: [encrypted 2d array]
+    """
+
+    # convert the given string to array of integers
+    list_of_integers = ConvertStringToInteger(base_table, string)
+
+    # create matrix from mapped list
+    matrix = createMatrix(base_table, list_of_integers)
+
+    #split the matrix into blocks of size 4
+    blocks = splitMatrix(matrix)
+
+    # calculate determinant of each block
     determinants = calculateDeterminantOfBlocks(blocks)
 
+    # create the final encoded matrix
     final_matrix = makeFinalMatrix(determinants, blocks)
 
     return final_matrix
-"""
-create the init matrix of the given string
-"""
-def createStringMatrix(base_table, given_string):
-    letters = list(given_string)
+pass
 
+def ConvertStringToInteger(base_table, given_string):
+    """convert the given string into array of integers using the base table
+
+    Args:
+        base_table ([dict]): [maps letters to integers]
+        given_string ([string]): [the string given to encode]
+
+    Returns:
+        [list]: [list of mapped chars of string into integers]
+    """
+
+    # blocks count is the number of divided blocks in the matrix, paper mentioned it as 'n'
     blocks_count = math.ceil(math.sqrt(len(given_string)))
     if( blocks_count % 2  == 1):
         blocks_count += 1
 
-    matrix = [ [ 0 for i in range(blocks_count) ] for j in range(blocks_count) ]
-    print(given_string)
+    # calculating the b variable, mentioned in the paper
+    if(blocks_count <= 3):
+        n = 3
+    else:
+        n = blocks_count
+
+    # replace spaces with zeros
+    given_string = given_string.replace(' ', '0')
+
+    letters = list(given_string)
+
+    # map letters to their integers
+    for i in range(len(letters)):
+        if(letters[i] in base_table):
+            # add b to each value of the base table
+            letters[i] = base_table[letters[i]] + n
+
+    return letters
+pass
+
+def createMatrix(base_table, integer_list):
+    """create the n*n matrix from the list of integers
+
+    Args:
+        base_table ([dict]): [maps letters to integers]
+        integer_list ([list]): [list of mapped string into integers]
+
+    Returns:
+        [list]: [2d matrix]
+    """
+    letters = list(integer_list)
+
+    # blocks count is the number of divided blocks in the matrix, paper mentioned it as 'n'
+    blocks_count = math.ceil(math.sqrt(len(integer_list)))
+    if( blocks_count % 2  == 1):
+        blocks_count += 1
+
+    # calculating the b variable, mentioned in the paper
+    if(blocks_count <= 3):
+        n = 3
+    else:
+        n = blocks_count
+
+    # making the 2d matrix, filling it with zeros (mapped value of it), putting list of mapped char in it
+    matrix = [ [ (base_table['0']+n) for i in range(blocks_count) ] for j in range(blocks_count) ]
     for i in range(blocks_count):
         for j in range(blocks_count):
             if(len(letters) > 0):
                 letter = letters.pop(0)
+
                 if( letter != ' '):
                     matrix[i][j] = letter
 
     return matrix
+pass
 
-def splitStringMatrix(matrix):
+def splitMatrix(matrix):
+    """split matrix into blocks of size 4
+
+    Args:
+        matrix ([list]): [2d matrix]
+
+    Returns:
+        [list]: [2d matrix of blocks]
+    """
     blocks_count = pow(int(len(matrix) / 2), 2)
 
+    # creating 2d list of blocks filled with zeros, finding proper block for each item in the matrix
     blocks =[ [ [ 0 for i in range(2) ] for j in range(2) ]  for j in range(blocks_count) ]
     for i in range(len(matrix[0])):
         for j in range(len(matrix[0])):
@@ -48,29 +126,17 @@ def splitStringMatrix(matrix):
             blocks[position_of_block][block_row][block_col] = matrix[i][j]
 
     return blocks
-
-def convertStringToInteger(base_table, given_string):
-    # n is the variable named by the paper
-    n = math.ceil(math.sqrt(len(given_string)))
-
-    # a simple condition to bind the n variable
-    if(n <= 3):
-        b = 3
-    else:
-        b = n
-
-    letters = list(given_string)
-
-    # map letters to their number
-    for i in range(len(letters)):
-        if(letters[i] in base_table):
-            if(letters[i] == ''):
-                continue
-            letters[i] = base_table[letters[i]] + b
-    print(letters)
-    return letters
+pass
 
 def calculateDeterminantOfBlocks(blocks):
+    """calculating determinant of each block
+
+    Args:
+        blocks ([list]): [list of blocks]
+
+    Returns:
+        [list]: [determinant of each block]
+    """
     determinants =[0]*len(blocks)
 
     # for each block, calculate the determinant
@@ -78,10 +144,20 @@ def calculateDeterminantOfBlocks(blocks):
         determinants[i] = blocks[i][0][0]*blocks[i][1][1] - blocks[i][0][1]*blocks[i][1][0]
 
     return determinants
+pass
 
 def makeFinalMatrix(determinants, blocks):
-    final_matrix = [ [ 0 for i in range(4) ] for j in range(len(blocks)) ]
+    """create final encoded matrix from blocks and determinants
 
+    Args:
+        determinants ([list]): [a list of determinants of each blocks]
+        blocks ([list]): [2d array of blocks]
+
+    Returns:
+        [list]: [2d encoded matrix]
+    """
+    # crete and fill the 2d matrix and encode the values in it
+    final_matrix = [ [ 0 for i in range(4) ] for j in range(len(blocks)) ]
     for i in range(len(blocks)):
         final_matrix[i][0] = determinants[i]
         final_matrix[i][1] = blocks[i][0][0]
@@ -89,3 +165,4 @@ def makeFinalMatrix(determinants, blocks):
         final_matrix[i][3] = blocks[i][1][1]
 
     return final_matrix
+pass
