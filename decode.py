785a20315ad7cc74ccfa81258f2c1789bc14980e
faramarz <farmamarzqoshchi@gmail.com>
2021-02-02 13:55:06 +0330

    adding reconstruction method

--- decode.py
+++ decode.py
@@ -1,11 +1,22 @@
+import math
+
 def do(base_table, matrix):
+
     q_matrix = calculateQMatrix(matrix)
     first_element_matrix = calculateFirstElementMatrix(q_matrix, matrix)
     second_element_matrix = calculateSecondElementMatrix(q_matrix, matrix)
-    findLatentIndexes(matrix, first_element_matrix, second_element_matrix, q_matrix)
+    latent_indexes = findLatentIndexes(matrix, first_element_matrix, second_element_matrix, q_matrix)
+    reshaped_blocks = reshapeBlocksWithLatentIndexes(latent_indexes, matrix)
+    reconstructed_matrix = reconstructMatrix(base_table, reshaped_blocks)
+    string = convertNumberMatrixToString(reconstructMatrix)
 
 def calculateQMatrix(matrix):
-    n = len(matrix)
+
+    if(len(matrix) <= 3):
+        n = 3
+    else:
+        n = len(matrix)
+
     q_matrix = [0]*4
     q_matrix = [fibonacci(n+1), fibonacci(n), fibonacci(n), fibonacci(n-1)]
     return q_matrix
@@ -19,25 +30,53 @@ def fibonacci(n):
 def calculateFirstElementMatrix(q_matrix, matrix):
     element_matrix = [0]*len(matrix)
     for i in range(len(element_matrix)):
-        element_matrix[i] = q_matrix[0]*matrix[i][0] + q_matrix[2]*matrix[i][1]
+        element_matrix[i] = q_matrix[0]*matrix[i][1] + q_matrix[2]*matrix[i][2]
 
     return element_matrix
 
 def calculateSecondElementMatrix(q_matrix, matrix):
     element_matrix = [0]*len(matrix)
     for i in range(len(element_matrix)):
-        element_matrix[i] = q_matrix[1]*matrix[i][0] + q_matrix[3]*matrix[i][1]
+        element_matrix[i] = q_matrix[1]*matrix[i][1] + q_matrix[3]*matrix[i][2]
 
     return element_matrix
 
 def findLatentIndexes(matrix, first_element_matrix, second_element_matrix, q_matrix):
     n = len(matrix)
-    latent_indexes = [0]*4
+    latent_indexes = [0]*len(matrix)
     for i in range(len(matrix)):
         temp_x = 0
         while True:
             if(pow(-1, n)*matrix[i][0] == ( first_element_matrix[i]*( q_matrix[1]*temp_x +  q_matrix[3]*matrix[i][3]) ) - ( second_element_matrix[i]*( q_matrix[0]*temp_x + q_matrix[2]*matrix[i][3] ) ) ):
-                print('s')
-
-            print(temp_x)
+                break;
             temp_x +=1
+        latent_indexes[i] = temp_x
+
+    return latent_indexes
+
+def reshapeBlocksWithLatentIndexes(latent_indexes, matrix):
+    reshaped_blocks = [ [ 0 for i in range(len(matrix[0])) ] for j in range(len(matrix)) ]
+
+    for i in range(len(matrix)):
+        reshaped_blocks[i][0] = matrix[i][1]
+        reshaped_blocks[i][1] = matrix[i][2]
+        reshaped_blocks[i][2] = latent_indexes[i]
+        reshaped_blocks[i][3] = matrix[i][3]
+
+    return reshaped_blocks
+
+def reconstructMatrix(base_table, reshaped_blocks):
+
+    blocks_per_row = int(math.sqrt(len(reshaped_blocks)))
+    blocks = blocks_per_row * 2
+
+    reconstructed_matrix = [ [ (base_table['0']+blocks) for i in range(blocks) ] for j in range(blocks) ]
+
+    for i in range(len(reshaped_blocks)):
+        for j in range(4):
+            row = int( 2*math.floor(i/blocks_per_row) + math.floor(j/2) )
+            col = int( 2*math.floor(i%blocks_per_row) + math.floor(j%2) )
+            reconstructed_matrix[row][col] = reshaped_blocks[i][j]
+
+    return reconstructed_matrix
+
